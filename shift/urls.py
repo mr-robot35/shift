1c6dc2b039b0a8eb78e1c6ec24d621cbdc051189
nakanoi <nakanoinryota@gmail.com>
2021-08-12 12:57:05 +0900

    adopted to docker

--- shift/urls.py
+++ shift/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from apply.urls import router
+from users.views import HomeView
 
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('users.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
