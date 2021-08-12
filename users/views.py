522325d3eea3c08935eee5a438f926f5588e0977
nakanoi <nakanoinryota@gmail.com>
2021-08-12 12:56:41 +0900

    add home page

--- users/views.py
+++ users/views.py
@@ -11,6 +11,12 @@ class SignupView(generic.CreateView):
     form_class = forms.CreateUserForm
     model = models.User
 
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['auth'] = self.request.user.is_authenticated
+
+        return context
+
     def get_success_url(self):
         return reverse_lazy('login')
     
@@ -26,6 +32,17 @@ class UserTopView(LoginRequiredMixin, generic.TemplateView):
         context = super().get_context_data()
         user = self.request.user
         context['user'] = user
+        context['auth'] = self.request.user.is_authenticated
 
+        return context
+
+
+class HomeView(generic.TemplateView):
+    template_name = 'users/home.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['auth'] = self.request.user.is_authenticated
+        
         return context
 
